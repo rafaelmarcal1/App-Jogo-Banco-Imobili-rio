9e9e9b5604d47cb4bff3b1475e928f9031a67b2b
Your Name <you@example.com>
2023-06-18 15:14:03 -0300

    Commit 1

---
+++ app/src/main/java/br/edu/ifsp/dmo/appexe2bancoimobiliario/MainActivity.java
@@ -0,0 +1,22 @@
+package br.edu.ifsp.dmo.appexe2bancoimobiliario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
